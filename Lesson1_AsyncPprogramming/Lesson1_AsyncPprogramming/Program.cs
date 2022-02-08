86da2ebd0183939b1ddf9d0b5a42d99f34379651
anvar89 <36979151+anvar89@users.noreply.github.com>
2022-02-08 20:41:19 +0300

    Implemented CancellationToken

--- Lesson1_AsyncPprogramming/Lesson1_AsyncPprogramming/Program.cs
+++ Lesson1_AsyncPprogramming/Lesson1_AsyncPprogramming/Program.cs
@@ -3,34 +3,48 @@ using System;
 using System.IO;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Lesson1_AsyncPprogramming
 {
     internal class Program
     {
+        static readonly CancellationTokenSource cts = new CancellationTokenSource();
         static HttpClient client = new HttpClient();
         static void Main(string[] args)
         {
-            string url = @"https://jsonplaceholder.typicode.com/posts/";
+            string url = "https://jsonplaceholder.typicode.com/posts/";
             string fileName = "result.txt";
             int startId = 4;
             int endId = 13;
 
-            DownloadPostsById(url, fileName, startId, endId).GetAwaiter().GetResult();
+            try
+            {
+                cts.CancelAfter(2000);
+                DownloadPostsById(url, fileName, startId, endId, cts.Token).GetAwaiter().GetResult();
+            }
+            catch(OperationCanceledException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cts.Dispose();
+            }
 
             Console.ReadKey();
         }
 
-        static async Task DownloadPostsById(string url, string fileName, int startId, int endId)
+        static async Task DownloadPostsById(string url, string fileName, int startId, int endId, CancellationToken token)
         {
 
             for (int id = startId; id <= endId; id++)
             {
                 try
                 {
-                    Post post = await GetPostAsync(new Uri(url + id));
-                    await WriteTextAsync(fileName, PostPrint(post));
+                    Post post = await GetPostAsync(new Uri(url + id), token);
+                    await WriteTextAsync(fileName, PostPrint(post), token);
                 }
                 catch (Exception ex)
                 {
@@ -41,10 +55,10 @@ namespace Lesson1_AsyncPprogramming
 
         }
 
-        static async Task<Post> GetPostAsync(Uri uri)
+        static async Task<Post> GetPostAsync(Uri uri, CancellationToken token)
         {
 
-            HttpResponseMessage responseMessage = await client.GetAsync(uri);
+            HttpResponseMessage responseMessage = await client.GetAsync(uri, token);
             responseMessage.EnsureSuccessStatusCode();
 
             string text = await responseMessage.Content.ReadAsStringAsync();
@@ -53,7 +67,7 @@ namespace Lesson1_AsyncPprogramming
 
         }
 
-        static async Task WriteTextAsync(string filePath, string text)
+        static async Task WriteTextAsync(string filePath, string text, CancellationToken token)
         {
             byte[] encodedText = Encoding.Unicode.GetBytes(text);
 
@@ -61,7 +75,7 @@ namespace Lesson1_AsyncPprogramming
                 FileMode.Append, FileAccess.Write, FileShare.None,
                 bufferSize: 4096, useAsync: true))
             {
-                await sourceStream.WriteAsync(encodedText, 0, encodedText.Length);
+                await sourceStream.WriteAsync(encodedText, 0, encodedText.Length, token);
             }
         }
 
